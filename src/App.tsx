cfa3a726bea13c3358466bcd5fdc80ada6b3fca4
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-08-27 16:30:27 +0700

    init code base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>React App with TypeScript</p>
+      <h2>Khoi dep trai</h2>
+    </div>
+  );
+}
+
+export default App;
