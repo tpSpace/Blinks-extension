c5218abfee12725016e3c3db7b4a8d12c6f3e6fc
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-08-27 17:47:31 +0700

    Add supported site

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
     <div>
       <h1>React App</h1>
       <p>React App with TypeScript</p>
-      <h2>Khoi dep trai</h2>
+      <h2>Khoi</h2>
     </div>
   );
 }
